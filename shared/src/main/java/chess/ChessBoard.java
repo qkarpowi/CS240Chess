ff3502be768debeca4f1f4cd90ff733a8a7466e8
Quinn Karpowitz <qkarpowi@byu.edu>
2024-01-26 08:02:20 -0700

    Final Final changes

---
+++ shared/src/main/java/chess/ChessBoard.java
@@ -0,0 +1,65 @@
+package chess;
+
+import java.util.Arrays;
+
+/**
+ * A chessboard that can hold and rearrange chess pieces.
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessBoard {
+    private ChessPiece[][] squares = new ChessPiece[8][8];
+    public ChessBoard() {
+        
+    }
+
+    /**
+     * Adds a chess piece to the chessboard
+     *
+     * @param position where to add the piece to
+     * @param piece    the piece to add
+     */
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+        squares[position.getRow() - 1][position.getColumn() - 1] = piece;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that = (ChessBoard) o;
+        return Arrays.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "squares=" + Arrays.deepToString(squares) +
+                '}';
+    }
+
+    /**
+     * Gets a chess piece on the chessboard
+     *
+     * @param position The position to get the piece from
+     * @return Either the piece at the position, or null if no piece is at that
+     * position
+     */
+    public ChessPiece getPiece(ChessPosition position) {
+        return squares[position.getRow() - 1][position.getColumn() - 1];
+    }
+
+    /**
+     * Sets the board to the default starting board
+     * (How the game of chess normally starts)
+     */
+    public void resetBoard() {
+        squares = new ChessPiece[8][8];
+    }
+}
---
+++ shared/src/main/java/chess/ChessPiece.java
@@ -0,0 +1,101 @@
+package chess;
+
+import chess.piececalculator.*;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+
+/**
+ * Represents a single chess piece
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessPiece {
+
+
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
+    public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
+
+        this.pieceColor = pieceColor;
+        this.type = type;
+    }
+
+    /**
+     * The various different chess piece options
+     */
+    public enum PieceType {
+        KING,
+        QUEEN,
+        BISHOP,
+        KNIGHT,
+        ROOK,
+        PAWN
+    }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "pieceColor=" + pieceColor +
+                ", type=" + type +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
+
+    /**
+     * @return Which team this chess piece belongs to
+     */
+    public ChessGame.TeamColor getTeamColor() {
+        return pieceColor;
+    }
+
+    /**
+     * @return which type of chess piece this piece is
+     */
+    public PieceType getPieceType() {
+        return type;
+    }
+
+    /**
+     * Calculates all the positions a chess piece can move to
+     * Does not take into account moves that are illegal due to leaving the king in
+     * danger
+     *
+     * @return Collection of valid moves
+     */
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        PieceMovesCalculator calculator;
+        if(type == PieceType.BISHOP){
+            calculator = new BishopCalculator();
+        } else if(type == PieceType.KING){
+            calculator = new KingCalculator();
+        } else if(type == PieceType.KNIGHT){
+            calculator = new KnightCalculator();
+        } else if(type == PieceType.PAWN){
+            calculator = new PawnCalculator();
+        } else if(type == PieceType.QUEEN){
+            calculator = new QueenCalculator();
+        } else if(type == PieceType.ROOK){
+            calculator = new RookCalculator();
+        } else {
+            return new HashSet<ChessMove>();
+        }
+
+        return calculator.pieceMoves(board, myPosition);
+    }
+}
